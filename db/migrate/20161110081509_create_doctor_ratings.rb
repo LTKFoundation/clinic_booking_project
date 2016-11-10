8a4ea0f2d115f78735efbbdb77375a0a57f06f44
J <kiendo95@gmail.com>
2016-11-10 15:21:27 +0700

    Generate DoctorComment

--- db/migrate/20161110081509_create_doctor_ratings.rb
+++ db/migrate/20161110081509_create_doctor_ratings.rb
@@ -1,9 +1,9 @@
 class CreateDoctorRatings < ActiveRecord::Migration[5.0]
   def change
     create_table :doctor_ratings do |t|
-      t.references :user, foreign_key: true
-      t.references :doctor, foreign_key: true
-      t.references :booking, foreign_key: true
+      t.references :user, foreign_key: true, index: true
+      t.references :doctor, foreign_key: true, index: true
+      t.references :booking, foreign_key: true, index: true
       t.integer :value
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161110081509) do
+ActiveRecord::Schema.define(version: 20161110081940) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,18 @@ ActiveRecord::Schema.define(version: 20161110081509) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["owner_id"], name: "index_clinics_on_owner_id", using: :btree
+  end
+
+  create_table "doctor_comments", force: :cascade do |t|
+    t.integer  "doctor_rating_id"
+    t.integer  "user_id"
+    t.integer  "doctor_id"
+    t.string   "content"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.index ["doctor_id"], name: "index_doctor_comments_on_doctor_id", using: :btree
+    t.index ["doctor_rating_id"], name: "index_doctor_comments_on_doctor_rating_id", using: :btree
+    t.index ["user_id"], name: "index_doctor_comments_on_user_id", using: :btree
   end
 
   create_table "doctor_ratings", force: :cascade do |t|
@@ -141,6 +153,9 @@ ActiveRecord::Schema.define(version: 20161110081509) do
 
   add_foreign_key "bookings", "schedules"
   add_foreign_key "bookings", "users"
+  add_foreign_key "doctor_comments", "doctor_ratings"
+  add_foreign_key "doctor_comments", "doctors"
+  add_foreign_key "doctor_comments", "users"
   add_foreign_key "doctor_ratings", "bookings"
   add_foreign_key "doctor_ratings", "doctors"
   add_foreign_key "doctor_ratings", "users"
