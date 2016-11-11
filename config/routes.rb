d87b5822b8f4db477f74c5b9a307a0827129a6a1
J <kiendo95@gmail.com>
2016-11-12 02:44:48 +0700

    skeleton page for new booking

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
 
   # Do not show all the users
-  resources :doctors, only: [ :show, :create, :update, :destroy ]
+  resources :doctors, only: [ :show, :create, :update, :destroy ] do
+    resources :gigs, only: [ ] do
+      resources :schedules, only: [] do
+        # Route to new booking with schedule params and client id
+        resources :bookings, only: [ :new, :create ]
+      end
+    end
+  end
 
   resources :clients, only: [ :show, :create, :update, :destroy ]
 
