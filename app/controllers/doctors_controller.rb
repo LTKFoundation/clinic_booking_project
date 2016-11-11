a07bcd45b2cc9dac967d1399954df8b6a76eef40
J <kiendo95@gmail.com>
2016-11-12 02:08:26 +0700

    Show page for doctor, no index route for doctor

---
+++ app/controllers/doctors_controller.rb
@@ -0,0 +1,13 @@
+class DoctorsController < ApplicationController
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  # Do not show all the users
+  resources :doctors, only: [ :show, :create, :update, :destroy ]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
 end
