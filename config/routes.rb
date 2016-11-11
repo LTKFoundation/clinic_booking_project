7858453d8e9cad49ee4e4845596eb9128b07c949
J <kiendo95@gmail.com>
2016-11-12 02:18:34 +0700

    Client show page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # Do not show all the users
   resources :doctors, only: [ :show, :create, :update, :destroy ]
 
+  resources :clients, only: [ :show, :create, :update, :destroy ]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
 end
