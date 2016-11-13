3f5553c1d890bc6bbfa927f1a900e85cd567fe34
Tu Chu Quang <tucq88@gmail.com>
2016-11-13 22:41:11 +0700

    Polish UI (#8)
    * Update UI login pagE
    
    * Update UI signup pagE
    
    * Update UI forgot page

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,13 @@ module ClinicBooking
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Overwrite Rails config for wrapping error field
+    # Read more http://stackoverflow.com/questions/5267998/rails-3-field-with-errors-wrapper-changes-the-page-appearance-how-to-avoid-t#5268106
+    Rails.application.config.action_view.field_error_proc = Proc.new do |html_tag, instance|
+      html_tag
+    end
+
   end
 end
+
