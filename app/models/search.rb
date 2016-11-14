b89f7511127514f4c818ee7eaaab45531ba45cdd
J <kiendo95@gmail.com>
2016-11-14 20:12:21 +0700

    Add model Search for all kind of queries

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,5 +1,9 @@
 class Search < ApplicationRecord
   def self.doctors(query)
+    search_doctors(query)
+  end
+
+  def self.search_doctors(query)
     if query.present?
       Doctor.search(query)
     end
