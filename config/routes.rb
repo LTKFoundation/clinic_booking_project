63840b8237f22d30f467c3b4e68439388267afee
Vu Long <vulong264@gmail.com>
2016-11-15 23:37:52 +0700

    added gigs page, working on schedules, need support

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,18 @@ Rails.application.routes.draw do
         # Route to new booking with schedule params and client id
         resources :bookings, only: [ :new, :create ]
       end
-    end
+    end 
   end
+
+	resources :doctors do
+		collection do
+			get 'add_clinic'
+		end
+		resources :gigs do
+			resources :schedules do
+			end
+		end
+	end
 
   resources :clients, only: [ :show, :create, :update, :destroy ]
 
