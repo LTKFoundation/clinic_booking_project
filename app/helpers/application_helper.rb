aebc7ee4c047ec141d8ba34c44b1945cae74a81f
J <kiendo95@gmail.com>
2016-11-18 00:10:57 +0700

    Use doctor.available?(expect_time) to deal with the datetime filter

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def no_doctors_check(doctors)
+    if doctors.nil? or doctors.length == 0
+      'Không tìm thấy bác sĩ nào!'
+    end
+  end
 end
--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,12 +1,20 @@
 class Schedule < ApplicationRecord
   belongs_to :gig
 
-  def closest
-    ("#{weekday} #{ start_at.to_s(:time) }").to_datetime
+  def closest(expect_time)
+    weekday_int = expect_time.cwday
+
+    addition = "#{weekday}".to_datetime.cwday - weekday_int
+    if addition < 0 then addition += 7 end
+
+    result = expect_time + addition.days
+    result.change({ hour: start_at.hour, min: start_at.min})
   end
 
   def available?(expect_time)
-    expect_time > closest
+    closest_available = closest(expect_time)
+    closest_available.to_date == expect_time.to_date and
+      closest_available > expect_time
   end
 
   def available_now?
