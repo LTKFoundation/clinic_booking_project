1f069afe1ca2a93981868fe6ba3214f32b585d7a
Tucq <tucq88@gmail.com>
2016-11-19 12:21:22 +0700

    Orgamize AppController a bit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,27 +2,25 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
-
   layout :layout_by_resource
 
   protected
 
-  def configure_permitted_parameters
-    # Permit needed attribute for sign_up
-    # Include uid, provider for FB, GG sign up
-    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
-      user_params.permit({ roles: [] }, :email, :password, :password_confirmation,
-           :name, :uid, :provider, :phone, :avatar,                         # similar fields
-           :gender, :address, :dob,                                                # users fields
-           :expertise, :description, :certificate, :verified_at)           # doctors fields
-    end
+    def configure_permitted_parameters
+      # Permit needed attribute for sign_up
+      # Include uid, provider for FB, GG sign up
+      devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+        user_params.permit({ roles: [] }, :email, :password, :password_confirmation,
+             :name, :uid, :provider, :phone, :avatar,                         # similar fields
+             :gender, :address, :dob,                                                # users fields
+             :expertise, :description, :certificate, :verified_at)           # doctors fields
+      end
 
-    # Permit attribute for editing
-    devise_parameter_sanitizer.permit(:account_update) do |user_params|
-      user_params.permit({ roles: [] }, :email, :current_password, :password, :password_confirmation, :name, :uid, :provider)
+      # Permit attribute for editing
+      devise_parameter_sanitizer.permit(:account_update) do |user_params|
+        user_params.permit({ roles: [] }, :email, :current_password, :password, :password_confirmation, :name, :uid, :provider)
+      end
     end
-
-    # Same as sign_in
 
     # Layout
     def layout_by_resource
@@ -33,5 +31,4 @@ class ApplicationController < ActionController::Base
       end
     end
 
-  end
 end
