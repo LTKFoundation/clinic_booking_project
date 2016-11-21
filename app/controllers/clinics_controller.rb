9b2caf2fe7d802ff74cb0324782240c53198694f
taduyvinh <taduyvinh@gmail.com>
2016-11-22 06:31:29 +0700

    Doctor can add clinic if not seeded

--- app/controllers/clinics_controller.rb
+++ app/controllers/clinics_controller.rb
@@ -1,48 +1,64 @@
 class ClinicsController < ApplicationController
-	@@cur_loc = nil
-	@@doctor_clinics = nil
-	def index
-		gon.watch.doctor_clinics = @@doctor_clinics
-		@@doctor_clinics = Clinic.all_clinic_with_gig
-		@all_clinic = @@doctor_clinics
-		logger.debug "GET INDEX"
-		# @@doctor_clinics = DoctorClinic.clinic_around(nil)
-	end
+  @@cur_loc = nil
+  @@doctor_clinics = nil
+  def index
+    gon.watch.doctor_clinics = @@doctor_clinics
+    @@doctor_clinics = Clinic.all_clinic_with_gig
+    @all_clinic = @@doctor_clinics
+    logger.debug "GET INDEX"
+    # @@doctor_clinics = DoctorClinic.clinic_around(nil)
+  end
 
-	def add_clinic_page
-		gon.watch.doctor_clinics = @@doctor_clinics
-		@@doctor_clinics = Clinic.clinic_around(nil)
-		logger.debug "GET ADD CLINIC PAGE"
-	end
+  def add_clinic_page
+    gon.watch.doctor_clinics = @@doctor_clinics
+    @@doctor_clinics = Clinic.clinic_around(nil)
+    logger.debug "GET ADD CLINIC PAGE"
+  end
 
-	def add_clinic
-		logger.debug "adding new Clinic => Name:"+params[:name]+
-		" Lat: "+ params[:latitude].to_s+
-		" Lng: "+ params[:longtitude].to_s
-		@clinic = Clinic.create!(doctor_id: params[:doctor],name: params[:name],address: params[:address], 
-		latitude: params[:latitude].to_s.to_f, longtitude: params[:longtitude].to_s.to_f)
-		unless @clinic
-			raise 'CAN NOT ADD CLINIC'
-		end
-		@clinic_near_by = Clinic.clinic_around(@@cur_loc)
-	end
+  def add_clinic
+    logger.debug "adding new Clinic => Name:"+params[:name]+
+    " Lat: "+ params[:latitude].to_s+
+    " Lng: "+ params[:longtitude].to_s
+    @clinic = Clinic.create!(doctor_id: current_doctor.id,name: params[:name],address: params[:address], 
+    latitude: params[:latitude].to_s.to_f, longtitude: params[:longtitude].to_s.to_f)
+    unless @clinic
+      raise 'CAN NOT ADD CLINIC'
+    end
+    @clinic_near_by = Clinic.clinic_around(@@cur_loc)
+  end
 
-	def clinic_near_by
-		logger.debug "Loading Clinics near Loc: "+@@cur_loc[0].to_s+"||"+@@cur_loc[1].to_s
-		@clinic_near_by = Clinic.clinic_around(@@cur_loc)
-	end
+  def clinic_near_by
+    logger.debug "Loading Clinics near Loc: "+@@cur_loc[0].to_s+"||"+@@cur_loc[1].to_s
+    @clinic_near_by = Clinic.clinic_around(@@cur_loc)
+  end
 
-	def current_loc
-		logger.debug "Receiving Current Loc"+params[:latitude].to_s+"||"+params[:longtitude].to_s
-		@@cur_loc = [params[:latitude], params[:longtitude]] 
-		logger.info "Current Log Received"+@@cur_loc[0].to_s+"||"+@@cur_loc[1].to_s
-		@@doctor_clinics = clinic_near_by
-	end
+  def current_loc
+    logger.debug "Receiving Current Loc"+params[:latitude].to_s+"||"+params[:longtitude].to_s
+    @@cur_loc = [params[:latitude], params[:longtitude]] 
+    logger.info "Current Log Received"+@@cur_loc[0].to_s+"||"+@@cur_loc[1].to_s
+    @@doctor_clinics = clinic_near_by
+  end
 
-	def sort_nearest
-		@@cur_loc = [params[:latitude], params[:longtitude]]
-		@@doctor_clinics = Clinic.find_by_sql ["SELECT name, latitude, longtitude, earth_distance(ll_to_earth( ?, ? ), 
-		ll_to_earth(clinics.latitude, clinics.longtitude)) as distance_from_current_location 
-		FROM clinics ORDER BY distance_from_current_location ASC", @@cur_loc[0].to_s.to_f, @@cur_loc[1].to_s.to_f]
-	end
+  def sort_nearest
+    @@cur_loc = [params[:latitude], params[:longtitude]]
+    @@doctor_clinics = Clinic.find_by_sql ["SELECT name, latitude, longtitude, earth_distance(ll_to_earth( ?, ? ), 
+    ll_to_earth(clinics.latitude, clinics.longtitude)) as distance_from_current_location 
+    FROM clinics ORDER BY distance_from_current_location ASC", @@cur_loc[0].to_s.to_f, @@cur_loc[1].to_s.to_f]
+  end
+
+def create
+  @clinic = Clinic.new clinic_params
+  if @clinic.save
+    @flash[:success] = "Created Clinic"
+    redirect_to clinics_path
+  else
+    @flash[:error] = "cant create clinic"
+    render add_clinic_page
+  end
+end
+
+def clinic_params
+  params.require(:clinic).permit(:doctor_id, :name, :address, :longtitude, :latitude, :photos)
+end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,23 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+  clinic_list = [
+    [ "Phòng khám Đa khoa Medelab", "86-88 Nguyễn Lương Bằng, Quang Trung, Đống Đa, Hà Nội", "21.018516", "105.842071", "https://dwbxi9io9o7ce.cloudfront.net/images/2015-12-11_020720.3832380000.jpeg"],
+    [ "Phòng khám Đa khoa Quốc tế, International SOS Hồ Chí Minh - Cơ sở 2", "10.78378", "106.69003", "https://dwbxi9io9o7ce.cloudfront.net/images/2015-12-02_071914.4940130000.jpeg" ],
+    [ "Phòng khám Đa khoa Quốc Tế VietSing", "21.025371", "105.843433", "https://dwbxi9io9o7ce.cloudfront.net/images/2015-11-27_182933.2461330000.jpeg" ],
+    [ "Phòng khám Đa khoa Quốc Tế VietSing", "21.025371", "105.843433", "https://dwbxi9io9o7ce.cloudfront.net/images/2015-11-27_182933.2461330000.jpeg" ],
+    [ "Phòng khám Vietlife MRI", "14 Trần Bình Trọng", "21.020742", "105.844517", "https://dwbxi9io9o7ce.cloudfront.net/images/2015-12-12_043002.6635410000.jpeg" ]
+  ]
 
+  clinic_list.each do |clinic|
+    Clinic.create( name: clinic[0], address: clinic[1], latitude: clinic[2], longtitude: clinic[3], photos: clinic[4] )
+  end
 
-## Create 10 users:
-["a", "b", "c", "d","e" ].each do |n|
-  User.create(
-    email: "#{n}@example.com",
-    password: "#{n.*6}",
-    name: "#{n*3}"
-  )
-end
-
-## Create 10 doctors:
-["g", "h", "i", "j",  "k"].each do |n|
-  Doctor.create(
-    email: "#{n}@example.com",
-    password: "#{n.*6}",
-    name: "#{n*3}",
-    expertise: 'Nhi'
-  )
-end
