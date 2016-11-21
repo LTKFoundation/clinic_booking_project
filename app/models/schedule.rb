a33c40ab7835cbb2f5009e65d59df9e64df43fbb
J <kiendo95@gmail.com>
2016-11-21 18:58:32 +0700

    Display 3 available schedules on doctor card

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -3,28 +3,31 @@ class Schedule < ApplicationRecord
 
   # ------------------------------------------------------------
   # METHOD USED FOR FILTER
+  # available if in the same date, and this schedule time occurs later than the expect_time
+  def available?(expect_time)
+    return false if start_at.nil? or start_at.empty?
+    return false if expect_time.cwday != weekday
+
+    start = start_at.to_datetime.utc.strftime( "%H%M%S%N" )
+    end_ = end_at.to_datetime.utc.strftime( "%H%M%S%N" )
+    expect = expect_time.utc.strftime( "%H%M%S%N" )
+
+    # result
+    start <= expect and expect <= end_
+  end
 
   # Closest datetime available of this schedule with expected time
   # If there is, then it's gonna be in the same day with expect_time
   def closest(expect_time)
     expected_wd = expect_time.cwday
-    binding.pry
-    addition = weekday - expected_wd
+    addition = weekday.to_int - expected_wd
     if addition < 0 then addition += 7 end
 
     result = expect_time + addition.days
     result.change({ hour: start_at.to_datetime.hour, min: start_at.to_datetime.min})
   end
 
-  # available if in the same date, and this schedule time occurs later than the expect_time
-  def available?(expect_time)
-    return false if start_at.nil? or !start_at.present? or start_at.empty?
-    closest_available = closest(expect_time)
-    closest_available.to_date == expect_time.to_date and
-      closest_available > expect_time
-  end
-
   def available_now?
-    closest < DateTime.now
+    available?(DateTime.now)
   end
 end
