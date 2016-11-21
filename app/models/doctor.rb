e9b763fc4eb3edea588844717aae0cfe8a786f71
J <kiendo95@gmail.com>
2016-11-21 19:15:28 +0700

    Update doctor card

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -25,6 +25,7 @@ class Doctor < ApplicationRecord
     associated_against: { clinics: [ :address ] },
     ignoring: :accents
 
+  # AVALABLE
   def available?(expect_time)
     gigs.each do |g|
       g.schedules.each do |s|
@@ -45,6 +46,11 @@ class Doctor < ApplicationRecord
     end
 
     return result
+  end
+
+  # HAS A GIG
+  def has_a_gig?
+    gigs.length > 0
   end
 
   private
