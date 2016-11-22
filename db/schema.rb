c1c45d91fe9b01f3b23f8023cda5850e169cb6ca
vulong264 <vulong264@gmail.com>
2016-11-22 20:49:42 +0700

    merged add-booking-dashboard-for-doctor with dev of milestones 2. Everything work for booking process and Doctor veridy Booking Code on Dashboard (#25)

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,8 @@ ActiveRecord::Schema.define(version: 20161121100847) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "cube"
+  enable_extension "earthdistance"
   enable_extension "unaccent"
 
   create_table "admins", force: :cascade do |t|
@@ -72,6 +74,7 @@ ActiveRecord::Schema.define(version: 20161121100847) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.float    "longtitude"
+    t.index "ll_to_earth(latitude, longtitude)", name: "clinic_loc_index", using: :gist
     t.index ["doctor_id"], name: "index_clinics_on_doctor_id", using: :btree
   end
 
@@ -150,10 +153,10 @@ ActiveRecord::Schema.define(version: 20161121100847) do
 
   create_table "schedules", force: :cascade do |t|
     t.integer  "gig_id"
-    t.text     "start_at"
     t.text     "end_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "start_at"
     t.integer  "weekday"
     t.index ["gig_id"], name: "index_schedules_on_gig_id", using: :btree
   end
@@ -184,10 +187,6 @@ ActiveRecord::Schema.define(version: 20161121100847) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                               null: false
     t.datetime "updated_at",                               null: false
-    t.string   "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
