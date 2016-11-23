1a55f616893bf753903877080037d99f6379ef84
J <kiendo95@gmail.com>
2016-11-24 05:15:20 +0700

    Client can add clinic, admin then have to verify the clinic

---
+++ app/controllers/admins/clinics_controller.rb
@@ -0,0 +1,23 @@
+class Admins::ClinicsController < Admins::AdminController
+  before_action :authenticate_admin!
+
+  def index
+    @clinics = Clinic.paginate(:page => params[:page], :per_page => 10).order('verified_at DESC')
+  end
+
+  def verify
+    @clinic = Clinic.find(params[:id])
+    if @clinic.nil?
+      flash[:error] = 'Không tìm thấy phòng khám!'
+    else
+      if !@clinic.verified_at.nil?
+        flash[:notice] = 'Đã xác nhận phòng khám này rồi!'
+      else
+        @clinic.update(verified_at: DateTime.now)
+        if @clinic.errors.any? then flash[:error] = 'Không thể xác nhận!' end
+      end
+    end
+
+    redirect_to admins_clinics_path
+  end
+end
