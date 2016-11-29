ae68069007b50329101032bae5c33b26e86b9379
Vu Long <vulong264@gmail.com>
2016-11-29 10:00:55 +0700

    update local schema.rb after pull from dev

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,11 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20161125151545) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "cube"
+  enable_extension "earthdistance"
   enable_extension "unaccent"
 
   create_table "admins", force: :cascade do |t|
@@ -74,6 +77,7 @@ ActiveRecord::Schema.define(version: 20161125151545) do
     t.datetime "updated_at",  null: false
     t.float    "longtitude"
     t.datetime "verified_at"
+    t.index "ll_to_earth(latitude, longtitude)", name: "clinic_loc_index", using: :gist
     t.index ["doctor_id"], name: "index_clinics_on_doctor_id", using: :btree
   end
 
@@ -169,12 +173,17 @@ ActiveRecord::Schema.define(version: 20161125151545) do
 
   create_table "schedules", force: :cascade do |t|
     t.integer  "gig_id"
-    t.text     "start_at"
     t.text     "end_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "start_at"
     t.integer  "weekday"
     t.index ["gig_id"], name: "index_schedules_on_gig_id", using: :btree
+  end
+
+  create_table "searches", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -198,10 +207,6 @@ ActiveRecord::Schema.define(version: 20161125151545) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                               null: false
     t.datetime "updated_at",                               null: false
-    t.string   "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
