0b6e1b87195a5154be7516eed5b02948a4c47fe8
Vu Long <vulong264@gmail.com>
2016-12-08 18:17:35 +0700

    finish uberdoctor displaying on map, worked on heroku

--- app/assets/javascripts/drlocs.js
+++ app/assets/javascripts/drlocs.js
@@ -2,11 +2,11 @@ function setupMap() {
     console.log("setup Map");
 }
 
-function sendLoc() {
+function sendLoc(lat, lng) {
     dr_name = $("input#uber_name").val();
     dr_id = $("input#uber_id").val();
-    lat = $("input#uber_lat").val();
-    lng = $("input#uber_lng").val();
+    // lat = $("input#uber_lat").val();
+    // lng = $("input#uber_lng").val();
     App.drlocs.send_loc({ command: "doctor_move", lat: lat, lng: lng, dr_name: dr_name, dr_id: dr_id });
 }
 $(document).on("turbolinks:load", setupMap)
