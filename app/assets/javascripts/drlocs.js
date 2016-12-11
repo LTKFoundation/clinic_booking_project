3aef2499b3556cb2421de3d5d137801d8821cb31
Vu Long <vulong264@gmail.com>
2016-12-11 09:01:57 +0700

    In addition to requiring a Uber Doctor, now User can raise a request with his/her budget, Doctors that are available can decide to pick this request or not

--- app/assets/javascripts/drlocs.js
+++ app/assets/javascripts/drlocs.js
@@ -37,6 +37,20 @@ function bookDoctor(dr_id, dr_name, lat, lng, cost) {
     request_uber(dr_id);
 }
 
+function serveThisRequest(user_id, lat, lng, user_name, user_phone, user_address, budget) {
+    dr_name = $("input#uber_name").val();
+    dr_id = $("input#uber_id").val();
+    dr_avatar = $("input#uber_avatar").val();
+    confirm = confirm("Are you sure you want to serve this request for " + budget + "?");
+    if (confirm) {
+        document.getElementById('user_request_name').innerHTML = "<span class='red-text text-darken-2'>" + user_name + "</span>";
+        document.getElementById('user_request_phone').innerHTML = "<span class='red-text text-darken-2'>" + user_phone + "</span>";
+        document.getElementById('user_request_address').innerHTML = "<span class='red-text text-darken-2'>" + user_address + "</span>";
+        document.getElementById('transaction_status').innerHTML = "<span class='green-text text-darken-2'>Request taken, waiting for user confirmation!</span>";
+        App.drlocs.take_request({ command: "take_request", dr_id: dr_id, dr_name: dr_name, user_id: user_id, dr_lat: $("input#uber_lat").val(), dr_lng: $("input#uber_lng").val() });
+    }
+}
+
 function showUserRequest(lat, lng, dr_id, user_id, user_name, user_phone, user_address) {
     doctor_txt = $('input[id=uber_id]');
     var my_id = doctor_txt.val();
@@ -61,6 +75,49 @@ function showUserRequest(lat, lng, dr_id, user_id, user_name, user_phone, user_a
     }
 }
 
+function showDoctorTakeMyRequest(dr_id, dr_name, user_id, dr_lat, dr_lng) {
+    my_id = $('input[id=user_request_id]').val();
+    if (my_id == user_id) {
+        my_budget = $('input[id=my_budget]').val();
+        document.getElementById('uber_doctor_confirm').innerHTML = "<span class='green-text text-darken-2'>Request accepted. Waiting for your confirmation!</span>";
+        document.getElementById('selected_uber_doctor').innerHTML = "<span class='red-text text-darken-2'>" + dr_name + "</span>";
+        document.getElementById('selected_uber_doctor_price').innerHTML = "<span class='red-text text-darken-2'>" + my_budget + "</span>";
+        $('input[id=selected_uber_doctor_id]').val(dr_id);
+        user_lat = $('input[id=user_request_lat]').val();
+        user_lng = $('input[id=user_request_lng]').val();
+        showRoute(dr_lat, dr_lng, user_lat, user_lng, uber_map);
+        alert("Doctor " + dr_name + " accepted your request. Can you confirm that he can come now?");
+        // enable confirm button now
+        $("#btn_confirm").prop("disabled", false);
+        $("#btn_reject").prop("disabled", false);
+    }
+}
+
+function responseToConfirm(accepted) {
+    dr_id = $('input[id=selected_uber_doctor_id]').val();
+    my_id = $('input[id=user_request_id]').val();
+    App.drlocs.response_dr_confirm({ command: "response_dr_confirm", dr_id: dr_id, user_id: my_id, confirmed: accepted });
+    if (accepted) {
+        document.getElementById('uber_doctor_confirm').innerHTML = "<span class='red-text green lighten-2'>Confirmed! Doctor is on his way</span>";
+    } else {
+        document.getElementById('uber_doctor_confirm').innerHTML = "<span class='white-text red lighten-2'>You canceled this request :( </span>";
+    }
+}
+
+function showUserResponse(dr_id, user_id, confirmed) {
+    var my_id = $('input[id=uber_id]').val();
+    if (my_id == dr_id) {
+        if (confirmed) {
+            alert("User confirmed! You can start driving now. Goodluck!");
+            document.getElementById('transaction_status').innerHTML = "<span class='red-text green lighten-2'>User confirmed! You can start driving now. Goodluck!</span>";
+            showRoute($("input#uber_lat").val(), $("input#uber_lng").val(), $('input[id=user_request_lat]').val(), $('input[id=user_request_lng]').val(), map);
+        } else {
+            alert("User rejected! We are very sorry for this inconvenience");
+            document.getElementById('transaction_status').innerHTML = "<span class='white-text red lighten-2'>User rejected! We are very sorry for this inconvenience</span>";
+        }
+    }
+}
+
 function showBookingConfirm(dr_id, user_id, dr_name) {
     uber_dr_id_txt = $('input[id=selected_uber_doctor_id]');
     doctor_id = uber_dr_id_txt.val();
@@ -79,4 +136,15 @@ function showBookingConfirm(dr_id, user_id, dr_name) {
     }
 }
 
+function userRaiseRequest() {
+    user_id = $("input#user_request_id").val();
+    user_name = $("input#user_request_name").val();
+    lat = $("input#user_request_lat").val();
+    lng = $("input#user_request_lng").val();
+    user_phone = $("input#user_request_phone").val();
+    user_address = $("#user_request_address").html();
+    budget = $('input[id=my_budget]').val();
+    document.getElementById('uber_doctor_confirm').innerHTML = "<span class='red-text text-darken-2'>Request raised. Waiting for doctors to accept!</span>";
+    App.drlocs.raise_request_uber({ command: "raise_request_uber", lat: lat, lng: lng, user_id: user_id, user_name: user_name, user_phone: user_phone, user_address: user_address, budget: budget });
+}
 $(document).on("turbolinks:load", setupMap)
--- app/channels/drlocs_channel.rb
+++ app/channels/drlocs_channel.rb
@@ -2,6 +2,7 @@
 class DrlocsChannel < ApplicationCable::Channel
   def subscribed
     stream_from "drlocs"
+    Rails.logger.info("subscribed to drlocs......")
   end
 
   def unsubscribed
@@ -24,4 +25,20 @@ class DrlocsChannel < ApplicationCable::Channel
     Rails.logger.info("confirming booking: #{data.inspect}")
     ActionCable.server.broadcast("drlocs",data)
   end
+
+  def raise_request_uber(data)
+    Rails.logger.info("user raising request #{data.inspect}")
+    ActionCable.server.broadcast("drlocs",data)
+  end
+
+  def take_request(data)
+    Rails.logger.info("a doctor is taking a request #{data.inspect}")
+    ActionCable.server.broadcast("drlocs",data)
+  end
+
+  def response_dr_confirm(data)
+    Rails.logger.info("User responsed to doctor confirmation #{data.inspect}")
+    ActionCable.server.broadcast("drlocs",data)
+  end
+
 end
